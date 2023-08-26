7bb10bcf7616f6ec02c6620b4958322bb1165acc
dev_chow <56135351+devchow@users.noreply.github.com>
2023-08-26 22:02:17 +0300

    update
    update

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const http = require('http')
 const port = 3000
 
 const server = http.createServer((req, res) => {
-  res.end('Hello Server!')
+//   res.end('Hello Server!')
+res.writeHead(200, {'Content-Type': 'text/html'})
+res.end('<h1>Hello Server!</h1>')
 })
 
+server.listen(port, function(error){
+    if(error){
+        console.log('Something went wrong', error)
+    }else{
+        console.log('Server is listening on port ' + port + " ✔")
+    }
+})
