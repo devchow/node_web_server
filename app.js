3d441d2ddba5a6235b6306043dcbed7707a7708c
dev_chow <56135351+devchow@users.noreply.github.com>
2023-08-26 22:06:35 +0300

    update
    update

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer((req, res) => {
-//   res.end('Hello Server!')
+// specify the type of data
 res.writeHead(200, {'Content-Type': 'text/html'})
+
+// read the index.html file
+fs.readFile('index.html', function(error, data){
+    // check for errors
+    if(error){
+        // display the error message on the terminal
+        res.writeHead(404)
+        res.write('Error: File Not Found')
+    }else{
+        // write content to the response body
+        res.write(data)
+    }
+})
+
 res.end('<h1>Hello Server!</h1>')
 })
 
